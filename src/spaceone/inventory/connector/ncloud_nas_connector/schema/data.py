cf19b6b6f90e9a70e0bc83ccf517c9112242ccee
so-jin <qrt56@naver.com>
2023-10-14 12:58:12 +0900

    rollback

--- src/spaceone/inventory/connector/ncloud_nas_connector/schema/data.py
+++ src/spaceone/inventory/connector/ncloud_nas_connector/schema/data.py
@@ -15,15 +15,15 @@ class NasVolume(Model):
     volume_total_size = IntType(serialize_when_none=False)
     volume_size = IntType(serialize_when_none=False)
     volume_use_size = IntType(serialize_when_none=False)
-    volume_use_ratio = FloatType(serialize_when_none=False)
-    snapshot_volume_configuration_ratio = FloatType(serialize_when_none=False)
-    snapshot_volume_config_time = IntType(serialize_when_none=False)
-    snapshot_volume_size = IntType(serialize_when_none=False)
-    snapshot_volume_use_size = IntType(serialize_when_none=False)
-    snapshot_volume_use_ratio = FloatType(serialize_when_none=False)
-    is_snapshot_configuration = BooleanType(serialize_when_none=False)
-    is_event_configuration = BooleanType(serialize_when_none=False)
-    is_return_protection = BooleanType(serialize_when_none=False)
+    # volume_use_ratio = FloatType(serialize_when_none=False)
+    # snapshot_volume_configuration_ratio = FloatType(serialize_when_none=False)
+    # snapshot_volume_config_time = IntType(serialize_when_none=False)
+    # snapshot_volume_size = IntType(serialize_when_none=False)
+    # snapshot_volume_use_size = IntType(serialize_when_none=False)
+    # snapshot_volume_use_ratio = FloatType(serialize_when_none=False)
+    # is_snapshot_configuration = BooleanType(serialize_when_none=False)
+    # is_event_configuration = BooleanType(serialize_when_none=False)
+    # is_return_protection = BooleanType(serialize_when_none=False)
 
 
     region_code = StringType(serialize_when_none=False)
@@ -33,8 +33,7 @@ class NasVolume(Model):
 
     nas_volume_instance_no = StringType(serialize_when_none=False)
     nas_volume_instance_status_name = StringType(serialize_when_none=False)
-    nas_volume_instance_description = StringType(serialize_when_none=False)
-    mount_information
+    # nas_volume_instance_description = StringType(serialize_when_none=False)
 
     mount_information = StringType(serialize_when_none=False)
 
