28c5c612a454e40f03448873064e69d6ecee8fed
sojin <qrt56@naver.com>
2023-10-22 18:04:12 +0900

    acl

--- src/spaceone/inventory/connector/ncloud_vpc_connector/connector.py
+++ src/spaceone/inventory/connector/ncloud_vpc_connector/connector.py
@@ -48,7 +48,6 @@ class VpcConnector(NCloudBaseConnector):
                 for vpc_instance in response_dict.get("vpc_list"):
 
                     vpc = VPC(self._create_model_obj(NcloudVPC,vpc_instance ))
-                    vpc.subnet = _subnet_instances
                     if hasattr(vpc, "vpc_no"):
                         vpc.subnet = self._find_objs_by_key_value( _subnet_instances,
                                                                    'vpc_no',
